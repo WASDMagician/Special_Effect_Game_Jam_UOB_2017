ec85e50a7e2141502b46c981bce856482f87684d
vinnchenzo94 <32128284+vinnchenzo94@users.noreply.github.com>
2017-11-02 12:27:55 +0000

    Transfer to vinnie machine

---
+++ Hallotween/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject move_object, turn_object;
+    public int health;
+    public float move_speed, turn_speed;
+    public Rigidbody player_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        player_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Handle_User_Input();
+	}
+
+    void Handle_User_Input()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forward, sideways, move;
+        forward = this.transform.forward * vertical * (move_speed * Time.deltaTime);
+        sideways = this.transform.right * horizontal * (move_speed * Time.deltaTime);
+        move = forward + sideways;
+        player_rigidbody.velocity = move;
+    }
+}
